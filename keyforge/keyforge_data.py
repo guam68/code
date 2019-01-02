6e078f370164826fb4f7a95877c936e674650651
guam68 <guam_68@hotmail.com>
2019-01-02 12:32:58 -0800

    get uniques update

--- keyforge/keyforge_data.py
+++ keyforge/keyforge_data.py
@@ -1,33 +1,63 @@
 import requests
 import re
 import json
+import time
+from time import sleep
+import ast
 
+start_time = time.time()
 page = '1'
-url = 'https://www.keyforgegame.com/api/decks/?page=' + page +'&page_size=25&links=cards'
+url = 'https://www.keyforgegame.com/api/decks/?page=' + page +'&page_size=25&links=cards&ordering=-date'
 
-# Get the total number of decks and the number of pages required to process
-response = requests.get(url).json()
-s = '\'count\': \d+'
-count = (re.findall(s, str(response))[0].split(':')[1]).strip()
-pages = int(count) // 25
-
-
-card_list = []
-# cards = response['_linked']['cards']
-   
-for i in range(-(-int(pages)//25)):
+# Get the number of pages required to process
+def get_num_pages(url):
+    response = requests.get(url).json()
+    s = '\'count\': \d+'
+    count = int((re.findall(s, str(response))[0].split(':')[1]).strip())
     
-    url = 'https://www.keyforgegame.com/api/decks/?page=' + page +'&page_size=25&links=cards'
-    data = requests.get(url).json()
-    cards = data['_linked']['cards']
+    with open('card_list2.txt', 'r', encoding='utf-8') as file:
+        old_count = file.readline()
 
-    for i, card in enumerate(cards):
-        if card not in card_list:
-            card_list.append(card)
+    new_count = count - int(old_count)
+    pages = -(-new_count // 25)
+    return pages, count
 
-    page = str(int(page) + 1)
 
-print(len(card_list))
+def get_unique_cards(page, url):
+    with open('card_list2.txt', 'r', encoding='UTF-8') as file:
+        file.readline()
+        card_list = ast.literal_eval(file.read())
 
-with open('card_list.txt', 'w', encoding='utf-8') as file:
-    file.write(str(card_list))
+    num_pages, deck_count = get_num_pages(url)
+    
+    for i in range(num_pages):
+        
+        data = requests.get(url).json()
+        cards = data['_linked']['cards']
+
+        for card in cards:
+            if card not in card_list:
+                card_list.append(card)
+
+        print(page, '/', num_pages)
+        page = str(int(page) + 1)
+        sleep(1)
+        
+
+    print(len(card_list))
+
+    with open('card_list2.txt', 'w', encoding='utf-8') as file:
+        file.write(str(deck_count) + '\n')
+        file.write(str(card_list))
+
+
+def get_decks():
+    deck_list = []
+    s = '\"data\":(.*?),\"_linked\"'
+    deck_list += re.findall(s, )
+
+
+get_unique_cards(page, url)
+
+print('Run Time: ', time.time() - start_time)
+
